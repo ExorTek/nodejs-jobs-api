6b0c7b48a042da06ffbfa4409ec6a4081efac320
ExorTek <exortek979@gmail.com>
2021-12-01 01:07:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const routers = require('./routers');
+const connectDatabase = require('./helpers/database/connectDatabase');
 const PORT = process.env.PORT;
 const app = express();
 dotenv.config({
     path: './config/env/config.env'
 });
+connectDatabase();
 app.use(express.json());
-app.use('/api', routers);
+app.use("/api", routers);
+
 app.listen(PORT, (() => {
     console.log(`Server listening on port ${PORT} : ${process.env.NODE_ENV}`);
 }));
