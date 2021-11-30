159533b9b82e503707864dd3cfc01cb20fcea063
ExorTek <exortek979@gmail.com>
2021-12-01 01:07:49 +0300

    Update jobs.js

--- routers/jobs.js
+++ routers/jobs.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { getAllJobs, getSingleJob, createJob, updateJob, deleteJob } = require('../controller/jobs');
+
+router.get('/', getAllJobs);
+router.get('/:id', getSingleJob);
+router.post('/', createJob);
+router.put('/:id', updateJob);
+router.delete('/:id', deleteJob);
+
+
 module.exports = router;
