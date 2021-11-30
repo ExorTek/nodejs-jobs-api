b23ea4673582a8ab236f1f8f9228c5b4044ea469
ExorTek <exortek979@gmail.com>
2021-12-01 01:07:56 +0300

    Update jobs.js

---
+++ controller/jobs.js
@@ -0,0 +1,24 @@
+const getAllJobs = async (req, res, next) => {
+
+};
+
+const getSingleJob = async (req, res, next) => {
+
+};
+const createJob = async (req, res, next) => {
+
+};
+const updateJob = async (req, res, next) => {
+
+};
+const deleteJob = async (req, res, next) => {
+
+};
+
+module.exports = {
+    getAllJobs,
+    getSingleJob,
+    createJob,
+    updateJob,
+    deleteJob
+};
