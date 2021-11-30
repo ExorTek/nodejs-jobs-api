db5fb4e7e65019b603fdf4d35fbfcae6bfe3d3e9
ExorTek <exortek979@gmail.com>
2021-12-01 01:07:53 +0300

    Update auth.js

--- routers/auth.js
+++ routers/auth.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { login, register,test } = require('../controller/auth');
+
+router.post('/register', register);
+router.post('/login', login);
+router.get('/test', test);
+
 module.exports = router;
