d6e348de07a472747d1dc2a68c340dfbb97cd4e4
ExorTek <exortek979@gmail.com>
2021-12-07 21:38:56 +0300

    Update User.js

--- model/User.js
+++ model/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new Schema({
     name: {
@@ -17,14 +19,14 @@ const UserSchema = new Schema({
     },
     role: {
         type: String,
-        enum: ['user', 'admin', 'expert'],
-        default: 'user'
+        enum: ['worker', 'admin', 'expert'],
+        default: 'worker'
     },
     password: {
+        select: false,
         type: String,
         minlength: 6,
-        required: [true, 'Please provide a password'],
-        select: false
+        required: [true, 'Please provide a password']
     },
     createdAt: {
         type: Date,
@@ -36,4 +38,30 @@ const UserSchema = new Schema({
         default: 'default.jpg'
     }
 });
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+    await bcrypt.genSalt(10, async (err, salt) => {
+        if (err) next(err);
+        await bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) next(err);
+            this.password = hash;
+            next();
+        });
+    });
+});
+UserSchema.methods.generateJwtFromUser = function () {
+    const { JWT_SECRET_KEY, JWT_EXPIRE } = process.env;
+    const payload = {
+        id: this._id,
+        name: this.name,
+        createdAt: this.createdAt
+    };
+    return jwt.sign(payload, JWT_SECRET_KEY, {
+        expiresIn: JWT_EXPIRE
+    });
+};
+
 module.exports = mongoose.model('User', UserSchema);
