d440e04b19df6becfc60262b63243f0777a350fd
ExorTek <exortek979@gmail.com>
2021-12-08 06:51:45 +0300

    added handler and created func

--- controller/jobs.js
+++ controller/jobs.js
@@ -1,19 +1,31 @@
-const getAllJobs = async (req, res, next) => {
+const Job = require('../model/Job');
+const expressAsyncHandler = require('express-async-handler');
+const CustomError = require("../helpers/error/CustomError");
 
-};
+const getAllJobs = expressAsyncHandler(async (req, res, next) => {
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
+    if (!jobs) {
+        return next(new CustomError('you don\'t seem to have a job'), 400);
+    }
+    return res.status(200).json({
+        success: true,
+        data: jobs,
+        count: jobs.length
+    });
+});
 
-const getSingleJob = async (req, res, next) => {
+const getSingleJob = expressAsyncHandler(async (req, res, next) => {
 
-};
-const createJob = async (req, res, next) => {
+});
+const createJob = expressAsyncHandler(async (req, res, next) => {
 
-};
-const updateJob = async (req, res, next) => {
+});
+const updateJob = expressAsyncHandler(async (req, res, next) => {
 
-};
-const deleteJob = async (req, res, next) => {
+});
+const deleteJob = expressAsyncHandler(async (req, res, next) => {
 
-};
+});
 
 module.exports = {
     getAllJobs,
