bc0dbc2289bd6d33d6dff0e3248d5ce2c58d652c
ExorTek <exortek979@gmail.com>
2021-12-08 06:52:19 +0300

    created reactui

---
+++ job-ui/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Login from "./component/login";
+function App () {
+    return (
+       <div className="container mx-auto">
+           <Login/>
+       </div>
+
+    );
+}
+
+export default App;
