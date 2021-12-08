ca2cc7d311bf3a94a57ca4684563783fd7ff10d8
ExorTek <exortek979@gmail.com>
2021-12-08 06:04:56 +0300

    Update Job.js

---
+++ model/Job.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const JobSchema = new Schema({
+    company: {
+        type: String,
+        required: [true, 'Please provide company name'],
+        minlength: [3, 'Company name must be greater than 3 characters']
+    },
+    position: {
+        type: String,
+        required: [true, 'Please provide position'],
+        maxlength: [3,'Position must be greater than 3 characters'],
+    },
+    status: {
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default: 'pending',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide user'],
+    },
+});
+module.exports = mongoose.model('Job', JobSchema);
