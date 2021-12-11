f7c9c967aed1d09dc94a705650878b6e27650dbc
ExorTek <exortek979@gmail.com>
2021-12-11 03:15:00 +0300

    Update App.js

--- job-ui/src/App.js
+++ job-ui/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./component/home";
 import Login from "./component/login";
+import Register from "./component/register";
+import Navbar from "./component/navbar";
+
 function App () {
     return (
-       <div className="container mx-auto">
-           <Login/>
+       <div>
+           <Navbar/>
+           <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="login" element={<Login />} />
+               <Route path="register" element={<Register />} />
+           </Routes>
        </div>
 
     );
