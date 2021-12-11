a467a0dacb5c6e9d12a02c73d5c66036befb7b19
ExorTek <exortek979@gmail.com>
2021-12-11 03:14:58 +0300

    Update login.js

--- job-ui/src/component/login.js
+++ job-ui/src/component/login.js
@@ -1,38 +1,62 @@
-export default function Login(){
-    return(
-        <div className="w-full max-w-xs">
+import axios from "axios";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Login () {
+    const [emailAddress, setEmailAddress] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = () => {
+        axios.interceptors.response.use((response) => {
+            return response?.data;
+        }, (error) => {
+            if (error) {
+                toast.error(error.response?.data?.message)
+            }
+        });
+        axios.post('http://localhost:5001/api/auth/login', {
+            email: emailAddress,
+            password: password
+        });
+    }
+    return (
+
+        <div className="container mx-auto items-center max-w-sm">
+            <ToastContainer/>
             <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                        Username
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
+                        Email
                     </label>
                     <input
+                        onChange={({ target }) => setEmailAddress(target.value)}
+                        value={emailAddress}
                         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="username" type="text" placeholder="Username"/>
+                        type="text" placeholder="Email"/>
                 </div>
                 <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
                         Password
                     </label>
                     <input
-                        className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password" type="password" placeholder="******************"/>
-                        <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                        onChange={({ target }) => setPassword(target.value)}
+                        value={password}
+                        className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        type="password" placeholder="***********"/>
                 </div>
-                <div className="flex items-center justify-between">
+                <div>
                     <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        onClick={login}
+                        className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 "
                         type="button">
-                        Sign In
+                        Login
                     </button>
-                    <p className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                       >
-                        Forgot Password?
-                    </p>
                 </div>
             </form>
             <p className="text-center text-gray-500 text-xs">
-                &copy;2020 Acme Corp. All rights reserved.
+                &copy;2021 Job
             </p>
         </div>
     );
