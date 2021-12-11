406514e4903e8653dc489302589fe50cba872c1c
ExorTek <exortek979@gmail.com>
2021-12-11 05:09:56 +0300

    added middleware to route

--- routers/jobs.js
+++ routers/jobs.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { getAllJobs, getSingleJob, createJob, updateJob, deleteJob } = require('../controller/jobs');
 const { getAccessToRoute } = require('../middleware/authorization/auth');
+const { checkJobExist } = require('../middleware/database/databaseErrorHelper');
+const { getJobOwnerAccess } = require('../middleware/authorization/auth');
 
-router.get('/', getAllJobs);
-router.get('/:id', getSingleJob);
+router.get('/', getAccessToRoute, getAllJobs);
+router.get('/:jobId', checkJobExist, getSingleJob);
 router.post('/', getAccessToRoute, createJob);
-router.put('/:id', updateJob);
-router.delete('/:id', deleteJob);
+router.put('/:jobId/edit', [getAccessToRoute, checkJobExist, getJobOwnerAccess], updateJob);
+router.delete('/:jobId/delete', [getAccessToRoute, checkJobExist, getJobOwnerAccess], deleteJob);
 
 
 module.exports = router;
