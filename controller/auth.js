5af9eddc03ee75a4c8164da4dd1078611be01e2b
ExorTek <exortek979@gmail.com>
2021-12-27 13:13:40 +0300

    Update auth.js

--- controller/auth.js
+++ controller/auth.js
@@ -1,35 +1,40 @@
-const User = require('../model/User');
-const expressAsyncHandler = require('express-async-handler');
+const User = require("../model/User");
+const expressAsyncHandler = require("express-async-handler");
 const CustomError = require("../helpers/error/CustomError");
-const { validateUserInput, comparePassword } = require('../helpers/lib/inputHelper');
-const { sendJwtToClient } = require('../helpers/authorization/tokenHelpers');
+const {
+  validateUserInput,
+  comparePassword,
+} = require("../helpers/lib/inputHelper");
+const { sendJwtToClient } = require("../helpers/authorization/tokenHelpers");
 
 const register = expressAsyncHandler(async (req, res, next) => {
-    const { name, email, password } = req.body;
-    const user = await User.create({
-        name,
-        email,
-        password
-    });
-    sendJwtToClient(user, res);
+  const { name, email, password } = req.body;
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  sendJwtToClient(user, res);
 });
 
 const login = expressAsyncHandler(async (req, res, next) => {
-    const { email, password } = req.body;
-    if (!validateUserInput(email, password)) {
-        return next(new CustomError('Please don\'t empty the password or email!', 400));
-    }
-    const user = await User.findOne({ email }).select("+password");
-    if (user === null) {
-        return next(new CustomError('User doesn\'t exist! Please register.'), 400);
-    }
-    if (!comparePassword(password, user.password)) {
-        return next(new CustomError('Please check the password!'), 400);
-    }
-    sendJwtToClient(user, res);
+  const { email, password } = req.body;
+  if (!validateUserInput(email, password)) {
+    return next(
+      new CustomError("Please don't empty the password or email!", 400)
+    );
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (user === null) {
+    return next(new CustomError("User doesn't exist! Please register."), 400);
+  }
+  if (!comparePassword(password, user.password)) {
+    return next(new CustomError("Please check the password!"), 400);
+  }
+  sendJwtToClient(user, res);
 });
 
 module.exports = {
-    register,
-    login
+  register,
+  login,
 };
