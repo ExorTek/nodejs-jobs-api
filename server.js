8a584a33c684dc49f5c3aaf8de0c2e3d89979513
ExorTek <exortek979@gmail.com>
2021-12-27 13:13:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require("cors")
-const routers = require('./routers');
-const connectDatabase = require('./helpers/database/connectDatabase');
-const customErrorHandler = require('./middleware/error/customErrorHandler');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const routers = require("./routers");
+const connectDatabase = require("./helpers/database/connectDatabase");
+const customErrorHandler = require("./middleware/error/customErrorHandler");
 
 dotenv.config({
-    path: './config/env/config.env'
+  path: "./config/env/config.env",
 });
 const PORT = process.env.PORT;
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 connectDatabase();
 app.use("/api", routers);
 app.use(customErrorHandler);
-app.listen(PORT, (() => {
-    console.log(`Server listening on port ${PORT} : ${process.env.NODE_ENV}`);
-}));
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT} : ${process.env.NODE_ENV}`);
+});
